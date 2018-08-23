fa4520bcc43ab37adf24054beafdb8d5fa361c47
scorewinner <tim@webkeller.ch>
2018-08-23 21:11:47 +0200

    change images

--- components/nav.php
+++ components/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <a class="navbar-brand" href="/">
-        <img src="../assets/images/logo-rounded.png" class="d-inline-block align-top nav-logo" alt="nav brand">
+        <img src="https://i.imgur.com/kHO4IKs.png" class="d-inline-block align-top nav-logo" alt="nav brand">
         scorewinner.ch
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
