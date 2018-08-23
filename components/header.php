fca60c09360c9e881485e6b3ed84370d4a7d3092
scorewinner <tim@webkeller.ch>
2018-08-23 21:01:21 +0200

    set shortcut icon

--- components/header.php
+++ components/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>scorewinner</title>
+    <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/18ENHCC.png"/>
     <link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
