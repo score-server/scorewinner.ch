9aad6f068168d1555f69706637c6fe14ddc60584
scorewinner <tim@webkeller.ch>
2018-08-27 12:13:11 +0200

    update frontpage

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include 'components/header.php'; ?>
 
 <div class="container col-md-12">
-    <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">scorewinner.ch</h1>
+    <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center col-md-12 col-12">
+        <h1>scorewinner.ch</h1>
         <p class="lead">We develop our own websites and web services. We also have some gameservers</p>
     </div>
 </div>
