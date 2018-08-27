3d1e344182966608bc2827ceafa516ecf427e080
scorewinner <tim@webkeller.ch>
2018-08-27 12:21:51 +0200

    removed local bootstrap & changed team page

--- pages/team.php
+++ pages/team.php
@@ -2,7 +2,60 @@
 
     <div class="container">
         <div class="body">
-            <h1>About Us</h1>
+            <h1>The Team</h1>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/19328039">
+                        <div class="card-body">
+                            <h5 class="card-title">Scorewinner</h5>
+                            <p>
+                                <small class="text-muted">Co-Founder, Developer</small>
+                            </p>
+                            <a href="https://github.com/Scorewinner">Github</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/25004579">
+                        <div class="card-body">
+                            <h5 class="card-title">Wetwer</h5>
+                            <p>
+                                <small class="text-muted">Co-Founder, Developer</small>
+                            </p>
+                            <a href="https://github.com/Wetwer">Github</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/24542471">
+                        <div class="card-body">
+                            <h5 class="card-title">Thellux</h5>
+                            <p>
+                                <small class="text-muted">Sponsor</small>
+                            </p>
+                            <a href="https://github.com/Thellux">Github</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="https://avatars1.githubusercontent.com/u/32476926">
+                        <div class="card-body">
+                            <h5 class="card-title">YKing</h5>
+                            <p>
+                                <small class="text-muted">Server Support</small>
+                            </p>
+                            <a href="https://github.com/YKaelin">Github</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <h3>Special Thanks to</h3>
+            <p>Scorecube</p>
         </div>
     </div>
 
