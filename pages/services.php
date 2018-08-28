b9ea10b70eba0fad119910a34d13090114868a6a
scorewinner <tim@webkeller.ch>
2018-08-28 15:36:09 +0200

    update status link

--- pages/services.php
+++ pages/services.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <div class="body">
-        <iframe src="http://movie.scorewinner.ch/status" width="100%" height="420px" style="border: none;"></iframe>
+        <iframe src="https://movie.scorewinner.ch/status" width="100%" height="420px" style="border: none;"></iframe>
     </div>
 </div>
 <?php include '../components/footer.php'; ?>
