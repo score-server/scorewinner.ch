4dff73bc340db11e0970f508bc7a1ed1d2d757d3
scorewinner <tim@webkeller.ch>
2018-09-26 08:46:25 +0200

    new login design

--- pages/login.php
+++ pages/login.php
@@ -4,14 +4,14 @@
     <div class="body text-center">
         <div>
             <form action="http://movie.scorewinner.ch/login" method="post"  class="form-signin">
-            	<img class="mb-4" src="../assets/images/logo.png" alt="" width="72" height="72">
+            	<img class="mb-4" src="../assets/images/logo_black.png" alt="" width="120px" height="120px">
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 
                 <label for="inputText" class="sr-only">Username</label>
-                <input type="text" class="form-control" name="name" placeholder="Username" required="" autofocus="">
+                <input type="text" class="form-control" name="name" placeholder="Username" required autofocus="">
 
                 <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required="">
+                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
 
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
             </form>
