9239cc6932f5466162b91ad9187fa5c20c8167be
scorewinner <tim@webkeller.ch>
2018-10-03 15:49:28 +0200

    added other services & ignore DS_Store

--- pages/services.php
+++ pages/services.php
@@ -2,12 +2,31 @@
 <?php include '../assets/functions/serverCheck.php'; ?>
 
 <div class="container">
-    <div class="body">
-        <h1>Shows the status of our services</h1>
-        <hr>
-        <div>
+    <div class="body row">
+        <div class="col-md-12">
+            <h1>Shows the status of our services</h1>
+            <hr>
+        </div>
+        <div class="col-md-6">
             <p class="lead">
-                IP Adress:<br>
+                Website:<br>
+                <code>scorewinner.ch</code>
+            </p>
+            <?php
+            $ip = "scorewinner.ch";
+            $port = 2500;
+            $status = checkServerStatus($ip, $port);
+            if ($status) {
+                echo '<p class="alert alert-success" role="alert">Service is online</p>';
+            } else {
+                echo '<p class="alert alert-danger" role="alert">Service is offline</p>';
+            }
+            ?>
+            <hr>
+        </div>
+        <div class="col-md-6">
+            <p class="lead">
+                Website:<br>
                 <code>movie.scorewinner.ch</code>
             </p>
             <?php
@@ -20,6 +39,41 @@
                 echo '<p class="alert alert-danger" role="alert">Service is offline</p>';
             }
             ?>
+            <hr>
+        </div>
+        <div class="col-md-6">
+            <p class="lead">
+                Website:<br>
+                <code>files.scorewinner.ch</code>
+            </p>
+            <?php
+            $ip = "scorewinner.ch";
+            $port = 8080;
+            $status = checkServerStatus($ip, $port);
+            if ($status) {
+                echo '<p class="alert alert-success" role="alert">Service is online</p>';
+            } else {
+                echo '<p class="alert alert-danger" role="alert">Service is offline</p>';
+            }
+            ?>
+            <hr>
+        </div>
+        <div class="col-md-6">
+            <p class="lead">
+                Website:<br>
+                <code>git.scorewinner.ch</code>
+            </p>
+            <?php
+            $ip = "scorewinner.ch";
+            $port = 3000;
+            $status = checkServerStatus($ip, $port);
+            if ($status) {
+                echo '<p class="alert alert-success" role="alert">Service is online</p>';
+            } else {
+                echo '<p class="alert alert-danger" role="alert">Service is offline</p>';
+            }
+            ?>
+            <hr>
         </div>
     </div>
 </div>
