8f798e3a571fe923623630c29c775cca853ce91b
Tim <tim@webkeller.ch>
2018-11-29 11:05:53 +0100

    fix header

--- components/header.php
+++ components/header.php
@@ -15,16 +15,6 @@
     <script type="text/javascript" src="../assets/js/steamprofiles.js"></script>
     <script type="text/javascript" src="../assets/js/app.js"></script>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130133062-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-130133062-1');
-    </script>
-
 </head>
 <body>
 <?php include 'nav.php'; ?>
