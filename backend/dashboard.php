7d09048e0d5b3296414900009f8b845c87b670c7
Tim <tim@webkeller.ch>
2018-12-10 15:46:36 +0100

    fixed footer in backend

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -1,6 +1,8 @@
 <?php include 'backend-header.php' ?>
-<div class="col-md-9">
-    <h2>Dashboard</h2>
-    <p>Here you are in the logged-in session of the website where you can find informations about some stuff</p>
+        <div class="col-md-9">
+            <h2>Dashboard</h2>
+            <p>Here you are in the logged-in session of the website where you can find informations about some stuff</p>
+        </div>
+    </div>
 </div>
 <?php include '../components/footer.php'; ?>
