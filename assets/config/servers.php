c67213cce714bb750e93d95a0826a79990923218
Tim <tim@webkeller.ch>
2019-06-06 11:19:07 +0200

    updated gameservers

--- assets/config/servers.php
+++ assets/config/servers.php
@@ -1,18 +1,13 @@
 <?php
 $servers = array(
     $server1 = array(
-        "1.12.2 Vanilla Minecraft",
+        "1.14.2 Vanilla Minecraft",
         "scorewinner.ch",
         25565,
     ),
     $server2 = array(
-        "1.13.1 Vanilla Minecraft",
+        "Unturned server",
         "scorewinner.ch",
-        25566,
-    ),
-    $server2 = array(
-        "1.7.10 Feed the Beast Infinity Evolved Skyblock",
-        "scorewinner.ch",
-        25567,
+        27015,
     ),
 );
